0f2fca3a8d19606d0b9fd38ffe5d30162786ee0e
Per <per@dubgeiser.net>
2023-12-01 08:49:20 +0100

    Day 1, part 1

--- 01/main.go
+++ 01/main.go
@@ -3,27 +3,57 @@ package main
 import (
 	"aoc2023/lib/file"
 	"fmt"
+	"strconv"
 )
 
-type Processor struct {
+type Solution1 struct {
+	value int
 }
 
-func (lp *Processor) ProcessLine(line string) {
+type Solution2 struct {
+	value int
 }
 
-func part1(input []int) int {
-	return 0
+func (lp *Solution1) ProcessLine(line string) {
+	snumber := ""
+	for i := 0; i < len(line); i++ {
+		if n, err := strconv.Atoi(string(line[i])); err == nil {
+			snumber = fmt.Sprint(n)
+			break
+		}
+	}
+	for i := len(line) - 1; i >= 0; i-- {
+		if n, err := strconv.Atoi(string(line[i])); err == nil {
+			snumber += fmt.Sprint(n)
+			break
+		}
+	}
+	if len(snumber) > 0 {
+		n, _ := strconv.Atoi(snumber)
+		lp.value += n
+	}
 }
 
-func part2(input []int) int {
-	return 0
+func (lp *Solution2) ProcessLine(line string) {
 }
 
-func main() {
-	lp := &Processor{}
-	lineCount, err := file.ReadLines("./input", lp)
+func part1() int {
+	s := &Solution1{}
+	lineCount, err := file.ReadLines("./input", s)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Read", lineCount, "lines")
+	return s.value
+}
+
+func part2() int {
+	s := &Solution2{}
+	file.ReadLines("./input", s)
+	return s.value
+}
+
+func main() {
+	fmt.Println(part1())
+	fmt.Println(part2())
 }
