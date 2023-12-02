dfd53b8db283c7876bd1fd0e4eaa72f5f0e50718
Per <per@dubgeiser.net>
2023-12-02 11:34:00 +0100

    Day 2, part 1

---
+++ 02/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"aoc2023/lib/file"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+var start = [3]int{12, 13, 14}
+
+type Solution1 struct {
+	value int
+}
+
+func isValidGrab(grab [3]int) bool {
+	return grab[0] <= start[0] && grab[1] <= start[1] && grab[2] <= start[2]
+}
+
+var color2index = map[string]int{"red": 0, "green": 1, "blue": 2}
+
+func parseGrab(sGrab string) [3]int {
+	grab := [3]int{0}
+	cc := strings.Split(sGrab, " ")
+	count, _ := strconv.Atoi(cc[0])
+	color := cc[1]
+	grab[color2index[color]] = count
+	return grab
+}
+
+func parseGrabs(sGrabs []string) [][3]int {
+	var grabs [][3]int
+	for _, g := range sGrabs {
+		grabs = append(grabs, parseGrab(g))
+	}
+	return grabs
+}
+
+func parseSets(setInput string) [][3]int {
+	var grabs [][3]int
+	sets := strings.Split(setInput, "; ")
+	for _, s := range sets {
+		for _, g := range parseGrabs(strings.Split(s, ", ")) {
+			grabs = append(grabs, g)
+		}
+	}
+	return grabs
+}
+
+func parseGame(line string) (int, [][3]int) {
+	var id int
+	var grabs [][3]int
+	fmt.Sscanf(line, "Game %d", &id)
+	grabs = parseSets(strings.Replace(line, fmt.Sprintf("Game %d: ", id), "", 1))
+	return id, grabs
+}
+
+func (lp *Solution1) ProcessLine(line string) {
+	id, grabs := parseGame(line)
+	isPossible := true
+	for _, grab := range grabs {
+		isPossible = isPossible && isValidGrab(grab)
+	}
+	if isPossible {
+		lp.value += id
+	}
+}
+
+func part1() int {
+	s := &Solution1{}
+	lineCount, err := file.ReadLines("./input", s)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Read", lineCount, "lines")
+	return s.value
+}
+
+func main() {
+	fmt.Println(part1())
+}
