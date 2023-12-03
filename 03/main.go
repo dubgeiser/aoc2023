398902f699215e99f0f452084399d085d8479d2d
Per <per@dubgeiser.net>
2023-12-03 13:14:00 +0100

    Day 3, part 2, I'M DYING OVER HERE

--- 03/main.go
+++ 03/main.go
@@ -7,6 +7,10 @@ import (
 	"strings"
 )
 
+type Pos struct {
+	row, col int
+}
+
 type Solution1 struct {
 	grid   [][]string
 	answer int
@@ -70,6 +74,54 @@ func (s *Solution1) Solve() {
 	}
 }
 
+func FindAdjacentGearPositions(grid [][]string, row, col int) map[Pos]bool {
+	adjacentGears := make(map[Pos]bool)
+	for _, dir := range directions {
+		checkPos := Pos{row + dir[0], col + dir[1]}
+		if !IsInGrid(grid, checkPos.row, checkPos.col) {
+			continue
+		}
+		if grid[checkPos.row][checkPos.col] == "*" {
+			adjacentGears[checkPos] = true
+		}
+	}
+	return adjacentGears
+}
+
+func (s *Solution1) Solve2() {
+	width := len(s.grid[0])
+	gears2AdjacentNumbers := make(map[Pos][]int)
+	for row := range s.grid {
+		currN := 0
+		adjacentGears := make(map[Pos]bool)
+		for col := range s.grid[row] {
+			c := s.grid[row][col]
+			isDigit := false
+			if n, err := strconv.Atoi(c); err == nil {
+				isDigit = true
+				currN *= 10
+				currN += n
+				for g := range FindAdjacentGearPositions(s.grid, row, col) {
+					adjacentGears[g] = true
+				}
+			}
+			if col == width-1 || !isDigit {
+				for gp := range adjacentGears {
+					gears2AdjacentNumbers[gp] = append(gears2AdjacentNumbers[gp], currN)
+				}
+				adjacentGears = nil
+				adjacentGears = make(map[Pos]bool)
+				currN = 0
+			}
+		}
+	}
+	for _, nums := range gears2AdjacentNumbers {
+		if len(nums) == 2 {
+			s.answer += nums[0] * nums[1]
+		}
+	}
+}
+
 func part1() {
 	s := &Solution1{}
 	lineCount, err := file.ReadLines("./input", s)
@@ -81,6 +133,14 @@ func part1() {
 	fmt.Println(s.answer)
 }
 
+func part2() {
+	s := &Solution1{}
+	file.ReadLines("./input", s)
+	s.Solve2()
+	fmt.Println(s.answer)
+}
+
 func main() {
 	part1()
+	part2()
 }
