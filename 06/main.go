7149d850b24297d621cc6eb87928b41213f936dd
Per <per@dubgeiser.net>
2023-12-07 12:54:45 +0100

    Solving day 6 as quadratic function
    This runs an order of magnitude faster; quick `time` of 20 runs showed
    ca. 10-20x.
    Not that it really mattered, because figuring this out took on order of
    magnitude longer than just bruteforce-running part2 :-D, but it was a
    fun exercise.
    
    The exact calculation of the interval threw me off, since I had not
    taken (record + 1) into account and only Floor()'d the interval ends.
    Which all worked just fine _except_ for the last race of the sample
    data.
    
    Thx to Jelle/Yasmin for reminding me of quadratic functions!

--- 06/main.go
+++ 06/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"aoc2023/lib/file"
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -80,7 +81,58 @@ func part2() {
 	fmt.Println(answer)
 }
 
+//
+// Given:
+//   - raceTime:   The time the race takes
+//   - startSpeed: The speed we're gonna start the race with (this is the same
+//     as the button press time)
+// We're looking for cases where startSpeed*(raceTime-startSpeed) > record
+// <=> startSpeed*(raceTime-startSpeed) - record > 0
+// <=> startSpeed*raceTime - startSpeed^2 - record > 0
+// <=> -startSpeed^2 + raceTime*startSpeed - record > 0
+//
+// Which can be written as quadratic function:
+//	ax^2 + bx + c = 0
+// where x = startSpeed, a = -1, b = raceTime and c = record
+//
+// If we solve for record+1 (because we want to beat the record)
+// we'll get 2 numbers representing an interval; the integers in that interval
+// are the starting speeds (or button hold times) that'll beat the given record.
+// substracting the smallest from the lowest yields the number of wins.
+// The SolveQuadratic solution runs 10-20 times faster
+//
+// <3 Jelle and Yasmin for reminding me of Quadratic function and Discriminant!
+//
+func SolveQuadratic(raceTime, record int) int {
+	a := -1.0
+	b := float64(raceTime)
+	c := float64(-(record + 1))
+	D := math.Pow(b, 2) - 4*a*c
+	x1 := int(math.Floor((-1*b - math.Sqrt(D)) / (2 * a)))
+	x2 := int(math.Ceil((-1*b + math.Sqrt(D)) / (2 * a)))
+	return x1 - x2 + 1
+}
+
+func part1a() {
+	s := &Solution1{}
+	file.ReadLines("./input", s)
+	answer := 1
+	for ti, raceTime := range s.times {
+		record := s.distances[ti]
+		answer *= SolveQuadratic(raceTime, record)
+	}
+	fmt.Println(answer)
+}
+
+func part2a() {
+	s := &Solution2{}
+	file.ReadLines("./input", s)
+	fmt.Println(SolveQuadratic(s.time, s.distance))
+}
+
 func main() {
 	part1()
 	part2()
+	part1a()
+	part2a()
 }
