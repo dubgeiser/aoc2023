a075a1eaacce89eb73f3fd186131372c9b4b359d
Per <per@dubgeiser.net>
2023-12-15 12:17:10 +0100

    Day 15, part 1
    Too easy, which means part2 is not for during lunch hour :-D

---
+++ 15/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"aoc2023/lib/file"
+	"fmt"
+	"strings"
+)
+
+type Solution struct {
+	answer1 int
+	answer2 int
+}
+
+func (s *Solution) ProcessLine(i int, line string) {
+	sequence := strings.Split(line, ",")
+	for _, step := range sequence {
+		s.answer1 += Hash(step)
+	}
+}
+
+func Hash(input string) int {
+	hash := 0
+	for _, step := range []byte(input) {
+		hash +=int(step)
+		hash = (hash * 17) % 256
+	}
+	return hash
+}
+
+func main() {
+	s := &Solution{}
+	lineCount, err := file.ReadLines("./input", s)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Read", lineCount, "lines")
+	fmt.Println(s.answer1)
+	fmt.Println(s.answer2)
+}
